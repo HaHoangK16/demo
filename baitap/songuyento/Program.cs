b144e8a3f1b1dede05619c44bf74b35b36434ee5
HaHoangK16 <hoang.hathucminh@gmail.com>
2020-09-08 10:31:00 +0700

    nop bai tap

---
+++ baitap/songuyento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace songuyento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //kiem tra so nguyen to
+            Console.WriteLine("Nhap so nguyen n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n < 2){
+                Console.WriteLine("Khong phai so nguyen to");
+            }else{
+                bool lasoNT = true;
+                for(int i = 2;i <= Math.Sqrt(n);i++)
+                {
+                    if(n % 1 == 0)
+                    {
+                        lasoNT = false;
+                    }
+                }
+                if(lasoNT)
+                {
+                    Console.WriteLine($"{n} la so nguyen to");
+                } else
+                {
+                    Console.WriteLine($"{n} khong phai la so nguyen to");
+                }
+            }
+            
+        }
+    }
+}
