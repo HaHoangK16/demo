560d58ea1da7d29d1f39bc13f3d78f260f4f6eaa
HaHoangK16 <70925614+HaHoangK16@users.noreply.github.com>
2020-09-09 19:16:14 +0700

    Add files via upload

--- sdts/Program.cs
+++ sdts/Program.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 
-namespace sdts
+namespace sdts_test
 {
     class Program
     {
+
+        float Area(float a, float b){
+            float result = a * b;
+            return result;
+        }
         static void Main(string[] args)
         {
             float width;
             float height;
-
+            float ketqua;
+            
             Console.Write("Enter width: ");
             width = float.Parse(Console.ReadLine());
             Console.Write("Enter height: ");
             height = float.Parse(Console.ReadLine());
+            Program n = new Program();
+            ketqua = n.Area(width,height);
+            Console.WriteLine("ket qua: {0}",ketqua);
+            Console.ReadLine();
 
-            float area = width * height;
-            Console.WriteLine("Area is: " + area);
         }
     }
 }
